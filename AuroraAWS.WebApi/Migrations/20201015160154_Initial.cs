b6fd794f69276cc37898359b704263ce1a1fef11
julianecheverriGL <julian.echeverri@gorillalogic.com>
2020-10-15 12:08:08 -0500

    initial migration

---
+++ AuroraAWS.WebApi/Migrations/20201015160154_Initial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuroraAWS.WebApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Associations");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
