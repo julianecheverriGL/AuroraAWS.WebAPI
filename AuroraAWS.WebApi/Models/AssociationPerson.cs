b08a4333001e692ed6363498799e58f870d46db9
julianecheverriGL <julian.echeverri@gorillalogic.com>
2020-10-15 12:11:58 -0500

    adding model

---
+++ AuroraAWS.WebApi/Models/AssociationPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuroraAWS.WebApi.Models
+{
+     [Table("AssociationsPersons")]
+    public class AssociationPerson
+    {
+         [Key]
+        public int Id { get; set; }
+
+        public int PersonId { get; set; }
+        public int AssociationId { get; set; }
+
+        public virtual Person Person { get; set; }
+        public virtual Association Association { get; set; }
+    }
+}
+
