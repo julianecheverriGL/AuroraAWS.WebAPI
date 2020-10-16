f4a09913cb14a8c8992e83762b5cef92421ce07b
julianecheverriGL <julian.echeverri@gorillalogic.com>
2020-10-16 12:43:44 -0500

    Changing connection to aurora

---
+++ AuroraAWS.WebApi/Migrations/20201016172526_initial.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuroraAWS.WebApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    ApplicationId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ApplicationCode = table.Column<byte>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.ApplicationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExternalUsersMap",
+                columns: table => new
+                {
+                    ExternalUserMapId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ExternalUserId = table.Column<int>(nullable: false),
+                    ExternalUserType = table.Column<byte>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalUsersMap", x => x.ExternalUserMapId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExternalUsersApplicationMap",
+                columns: table => new
+                {
+                    ExternalUserApplicationMapId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ExternalUserMapId = table.Column<int>(nullable: false),
+                    ApplicationId = table.Column<int>(nullable: false),
+                    MetaData = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalUsersApplicationMap", x => x.ExternalUserApplicationMapId);
+                    table.ForeignKey(
+                        name: "FK_ExternalUsersApplicationMap_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "ApplicationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExternalUsersApplicationMap_ExternalUsersMap_ExternalUserMap~",
+                        column: x => x.ExternalUserMapId,
+                        principalTable: "ExternalUsersMap",
+                        principalColumn: "ExternalUserMapId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExternalUsersApplicationMap_ApplicationId",
+                table: "ExternalUsersApplicationMap",
+                column: "ApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExternalUsersApplicationMap_ExternalUserMapId",
+                table: "ExternalUsersApplicationMap",
+                column: "ExternalUserMapId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExternalUsersApplicationMap");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+
+            migrationBuilder.DropTable(
+                name: "ExternalUsersMap");
+        }
+    }
+}
--- AuroraAWS.WebApi/Models/MetaDataDBContext.cs
+++ AuroraAWS.WebApi/Models/MetaDataDBContext.cs
@@ -19,13 +19,13 @@ namespace AuroraAWS.WebApi.Models
             if (!optionsBuilder.IsConfigured)
             {
                 // Aurora/MySql
-                // optionsBuilder.UseMySql("server=metadatacluster-instance-1.ciwawmmpzleq.us-east-1.rds.amazonaws.com;database=MetaDataDBAurora;uid=admin;pwd=Admin123*", x => x.ServerVersion("5.7.12-mysql"));
+                optionsBuilder.UseMySql("server=metadatacluster-instance-1.cbidsjnqo9ij.us-east-1.rds.amazonaws.com;database=MetaDataDB;uid=admin;pwd=Admin123*", x => x.ServerVersion("5.7.12-mysql"));
 
                 // CrossTenancyDatabase
                 // optionsBuilder.UseMySql("server=cross-tenancy-database-instance-1.ch00xfv5agd5.us-east-2.rds.amazonaws.com;database=CrossTenancyDatabase;uid=admin;pwd=Admin123*", x => x.ServerVersion("5.7.12-mysql"));
 
                 // MySql
-                optionsBuilder.UseMySql("server=localhost;database=MetaDataMySQLDocker;uid=root;pwd=password", x => x.ServerVersion("8.0.20-mysql"));
+                // optionsBuilder.UseMySql("server=localhost;database=MetaDataMySQLDocker;uid=root;pwd=password", x => x.ServerVersion("8.0.20-mysql"));
             }
         }
 
